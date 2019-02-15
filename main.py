a52a8f15a1680f96fc61f1027fa4bc626b200d7d
Zein Ersyad <zeinersyad@gmail.com>
2019-02-15 13:47:06 +0700

    Done

---
+++ main.py
@@ -0,0 +1,152 @@
+import csv
+from functools import reduce
+
+class Model :
+    def __init__(self, id, age, workclass, education, marital_status, occupation, relationship, hpw, income):
+        self.id = id
+        self.age = age
+        self.workclass = workclass
+        self.education = education
+        self.marital_status = marital_status
+        self.occupation = occupation
+        self.relationship = relationship
+        self.hpw = hpw
+        self.income = income
+
+    def addIncome(self, income):
+        self.income = income
+
+#--==============================^^Model^^==============================--
+
+attributes = [[[],[],[]], [[],[],[]], [[],[],[]], [[],[],[]], [[],[],[]], [[],[],[]], [[],[],[]], [[],[]]]
+models_train = []
+model_arrays_train = []
+index_b = [0,0,0,0,0,0,0,0]
+temp_50K = []
+
+b_bawah = [0,0]
+models_test = []
+
+def importTrain():
+    with open('res/TrainsetTugas1ML.csv') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=',')
+        line_count = 0
+        for row in spamreader:
+            if line_count == 0:
+                line_count += 1
+            else:
+                models_train.append(Model(row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8])) # pemodelan
+                model_arrays_train.append([row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8]]) # untuk permudah index
+                line_count += 1
+    for model in models_train:
+        setAttribute(0,model.age)
+        setAttribute(1,model.workclass)
+        setAttribute(2,model.education)
+        setAttribute(3,model.marital_status)
+        setAttribute(4,model.occupation)
+        setAttribute(5,model.relationship)
+        setAttribute(6,model.hpw)
+        setAttribute(7,model.income)
+
+def importTest():
+    with open('res/TestsetTugas1ML.csv') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=',')
+        line_count = 0
+        for row in spamreader:
+            calculation_p_more50  = []
+            calculation_p_less50  = []
+            if line_count == 0:
+                line_count += 1
+            else:
+                for i in range(1, 8):
+                    for j in range(0,3):
+                        if(row[i] == attributes[i-1][j][0]):
+                            # print(row[0], row[i], attributes[i-1][j][0])
+
+                            calculation_p_more50.append(attributes[i-1][j][1])
+                            calculation_p_less50.append(attributes[i-1][j][2])
+                            
+                calculation_p_less50.append(attributes[7][1][1])
+                calculation_p_more50.append(attributes[7][0][1])
+
+                multiPmore = multiplyList(calculation_p_more50)
+                multiPless = multiplyList(calculation_p_less50)
+
+                if(multiPless > multiPmore):
+                    models_test.append([row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], '<=50k'])
+                else:
+                    models_test.append([row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], '>50k'])
+
+                line_count += 1
+
+def csv_writer():
+    with open('TebakanTugas1ML.csv', 'w') as csvfile:
+        writer = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        writer.writerow(['id', 'age', 'workclass', 'education', 'marital-status', 'occupation', 'relationship', 'hours-per-week'])
+        for test in models_test:
+            writer.writerow([test[0], test[1], test[2], test[3], test[4], test[5], test[6], test[7], test[8]])
+
+def setAttribute(index, model):
+    isAppend = True
+    k = index_b[index]
+    if(index_b[index] > 2): k = 2
+
+    for attribute in attributes[index]:
+        for att in attribute:
+            if(att == model):
+                isAppend = False
+                break
+    if(isAppend):
+        attributes[index][k].append(model)
+        index_b[index] += 1
+
+def getProbs(l, k):
+    maxK = 2
+    if(k > 2): getProbs(l+1, 0)
+    if(l > 7): return
+    if(l == 7): maxK = 1
+    if(k > maxK): return
+    sizeData = [0, 0]
+    
+    if(l != 7):
+        for model in model_arrays_train:
+            if((model[l+1] == attributes[l][k][0]) and (model[8] == attributes[7][0][0])): # >50k
+                sizeData[0]+=1
+            elif((model[l+1] == attributes[l][k][0]) and (model[8] == attributes[7][1][0])): # <=50k
+                sizeData[1]+=1
+        attributes[l][k].append(round(float(float(sizeData[0])/float(b_bawah[0])),2))
+        attributes[l][k].append(round(float(float(sizeData[1])/float(b_bawah[1])),2))
+        
+    else:
+        if(k == 1):
+            attributes[l][k-1].append(float(b_bawah[0])/float(b_bawah[0]+b_bawah[1]))
+            attributes[l][k].append(float(b_bawah[1])/float(b_bawah[0]+b_bawah[1]))
+            
+    getProbs(l, k+1)
+
+def getProbsBawah():
+    for model in model_arrays_train:
+        if(model[8] == attributes[7][0][0]): # >50k
+            b_bawah[0]+=1
+        elif(model[8] == attributes[7][1][0]): # <=50k
+            b_bawah[1]+=1
+
+# https://www.geeksforgeeks.org/python-multiply-numbers-list-3-different
+def multiplyList(myList) : 
+    result = 1
+    for x in myList: 
+         result = result * x  
+    return result  
+
+def main():
+    importTrain()
+    getProbsBawah()
+    getProbs(0, 0)
+    importTest()
+    csv_writer()
+    # print(attributes[7][0][1])
+    # for attribute in attributes:
+    #     print(attribute)        
+
+if __name__ == "__main__":
+    main()
